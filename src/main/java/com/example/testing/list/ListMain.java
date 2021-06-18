104b492d24c77695eca20758524fe6655fa45acf
dasWarder <Al.94_94@mail.ru>
2021-06-18 12:08:06 +0300

    Add implementation of doubly LinkedList

--- src/main/java/com/example/testing/list/ListMain.java
+++ src/main/java/com/example/testing/list/ListMain.java
@@ -1,18 +1,37 @@
 package com.example.testing.list;
 
-import com.example.testing.list.linkedList.SimpleLinkedList;
+import com.example.testing.list.linkedList.DoublyLinkedList;
+
+import java.util.LinkedList;
 
 public class ListMain {
 
     public static void main(String[] args) {
-        SimpleLinkedList linkedList = new SimpleLinkedList();
 
-        linkedList.insertFirst(12);
-        linkedList.insertFirst(25);
-        linkedList.insertFirst(89);
+        DoublyLinkedList<Long> linkedList = new DoublyLinkedList<>();
 
-        System.out.println("Removed: " + linkedList.remove(89));
+        linkedList.insertHead(1L);
+        linkedList.insertHead(2L);
+        linkedList.insertHead(3L);
 
+        System.out.println("Print all: ");
         linkedList.printAll();
+
+        linkedList.insertTail(100L);
+        linkedList.insertTail(101L);
+
+        System.out.println("Print all after changing: ");
+        linkedList.printAll();
+
+        System.out.println("Index of element: " + linkedList.indexOf(1L));
+
+        System.out.println("Removed head: " + linkedList.removeHead());
+        linkedList.printAll();
+
+        System.out.println("Removed tail: " + linkedList.removeTail());
+        linkedList.printAll();
+
+
+
     }
 }
---
+++ src/main/java/com/example/testing/list/linkedList/DoublyLinkedList.java
@@ -0,0 +1,149 @@
+package com.example.testing.list.linkedList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class DoublyLinkedList<T> {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private class Link<T> {
+
+        private T value;
+
+        private Link<T> prev;
+
+        private Link<T> next;
+
+        public Link(T value) {
+            this.value = value;
+        }
+    }
+
+    private Link<T> head;
+
+    private Link<T> tail;
+
+    private int length;
+
+    public int indexOf(T element) {
+
+        int index = length;
+
+        for(Link<T> temp = tail; temp != null; temp = temp.getPrev()) {
+            index--;
+            if(temp.getValue().equals(element)) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    public void insertHead(T element) {
+
+        Link<T> newHead = new Link<T>(element);
+        newHead.setNext(head);
+
+        if (isEmpty()) {
+            tail = newHead;
+        }
+
+        head = newHead;
+        length++;
+    }
+
+    public void insertTail(T element) {
+
+        Link<T> newTail = new Link<>(element);
+
+        newTail.setPrev(tail);
+
+        if(isEmpty()) {
+
+            head = newTail;
+
+        } else {
+
+            tail.setNext(newTail);
+
+        }
+
+        tail = newTail;
+        length++;
+    }
+
+    public T removeHead() {
+
+        Link<T> nextLink = head.getNext();
+
+        T removedValue = validationAndNullingLinks(nextLink, head);
+        head = nextLink;
+
+        length--;
+
+        return removedValue;
+    }
+
+    public T removeTail() {
+
+        Link<T> prevLink = tail.getPrev();
+
+        T removedValue = validationAndNullingLinks(prevLink, tail);
+
+        prevLink.setNext(null);
+        tail = prevLink;
+
+        length--;
+
+        return removedValue;
+    }
+
+    public void printAll() {
+
+        Link<T> tempLink = head;
+
+        while(tempLink != null) {
+
+            System.out.println(tempLink.getValue());
+
+            tempLink = tempLink.getNext();
+        }
+
+    }
+
+    public boolean isEmpty() {
+
+        return head == null;
+    }
+
+    public int size() {
+        return length;
+    }
+
+    private T validationAndNullingLinks(Link<T> newLink, Link oldLink) {
+
+        if(!isEmpty()) {
+
+            T value = (T) oldLink.getValue();
+
+            if (tail == head) {
+
+                tail = null;
+                head = null;
+
+            } else {
+                oldLink.setNext(null);
+                oldLink.setPrev(null);
+                oldLink.setValue(null);
+            }
+
+            return value;
+
+        }
+
+        return null;
+    }
+}
