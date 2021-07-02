ff8334bc968c4abddc40c431872165798ae743cd
dasWarder <Al.94_94@mail.ru>
2021-07-02 10:28:26 +0300

    Implement solution of a backpack task variation

---
+++ src/main/java/com/example/testing/recursion/BackpackAlgorithm.java
@@ -0,0 +1,66 @@
+package com.example.testing.recursion;
+
+public class BackpackAlgorithm {
+
+    private static int start = 0;
+
+    public static void main(String[] args) {
+
+        int[] backpack = new int[] { 11, 8, 7, 6, 5 };
+        int weight = 45;
+        int sum = 0;
+
+       sum = find(backpack, weight, 0, 0);
+       System.out.println(sum);
+    }
+
+
+    private static int find(int[] backpack, int weight, int sum, int index) {
+
+        if(sum == weight) {
+
+            return sum;
+
+        }
+
+        if(index >= backpack.length) {
+
+            return 0;
+
+        }
+
+        int branch = recursionFind(backpack, weight, sum, index);
+
+        return branch == 0? find(backpack, weight, sum, index + 1) : branch;
+    }
+
+    
+    private static int recursionFind(int[] backpack, int weight, int sum, int index) {
+
+        if(sum == weight) {
+
+            return sum;
+
+        }
+
+        if(index == backpack.length) {
+
+            return 0;
+        }
+
+        if(sum < weight) {
+
+            sum = sum + backpack[index];
+
+        } else {
+
+            sum = sum - backpack[index - 1];
+            index--;
+
+        }
+
+        return recursionFind(backpack, weight, sum, index + 1);
+    }
+
+
+}
